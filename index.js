6b202239350612e96aa35b58275645a75dd40198
Abhinav Kanoongo <102249054+1117DataStock@users.noreply.github.com>
2024-05-31 23:52:48 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const input = document.getElementById("numOfWords");
+const container = document.querySelector(".container");
+
+const generatedWord  = (n) =>{
+    let text = "";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+
+    for(let i=0; i<n; i++){
+        const random = (Math.random() *25).toFixed(0);
+        text += letters[random];
+    }
+    return text;
+};
+
+// console.log(generatedWord(5));
+
+
+let numOfWords;
+const generate = () =>{
+    numOfWords = (Number(input.value));
+    const para = document.createElement("p");
+
+    let data = "";
+    for(let i=0; i<numOfWords; i++){
+        const randomNumber = (Math.random()*15).toFixed(0);
+        data +=generatedWord(randomNumber);
+        data += " ";
+    }
+
+    para.innerText = data;
+    para.setAttribute("class" , "paras");
+    container.append(para);
+};
